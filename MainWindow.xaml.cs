6c16fa760dee0320f65b353fe062a59542b032f0
Sqwan2 <133329601+Sqwan2@users.noreply.github.com>
2023-05-15 13:05:58 +0200

    Update MainWindow.xaml.cs
    Trickeln über Simulator hinzugefügt.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,13 +2,18 @@ using System;
 using System.Windows;
 using System.Windows.Input;
 using System.IO.Ports;
-
+using System.Threading;
+using System.Windows.Controls;
 
 namespace AutoTricklerGui
 {
     public partial class MainWindow : Window
     {
         private int count = 0;
+        static SerialPort _serialPort;
+        private delegate void SetTextDeleg(string text);
+        decimal scaleValue = 0;
+        public bool isEnabled = false;
 
         public MainWindow()
         {
@@ -19,20 +24,51 @@ namespace AutoTricklerGui
                 comPorts.Items.Add(port);
                 comPortsTrickler.Items.Add(port);
             }
+
+            _serialPort = new SerialPort("COM6", 9600, Parity.None, 8, StopBits.One);
+            _serialPort.Handshake = Handshake.None;
+            _serialPort.ReadTimeout = 500;
+            _serialPort.WriteTimeout = 500;
+            _serialPort.DataReceived += new SerialDataReceivedEventHandler(sp_DataReceived);
+            _serialPort.Open();
+
+        }
+
+        void sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string scaleValue = _serialPort.ReadLine();
+            Dispatcher.BeginInvoke(new SetTextDeleg(si_DataReceived), new object[] { scaleValue });
+        }
+        private void si_DataReceived(string data) 
+        {
+            scaleValue = Convert.ToDecimal(data.Trim());
+            scaleValueLabel.Text = data.Trim();
         }
 
         private void Start_Button_Click(object sender, RoutedEventArgs e)
         {
-            double powderQtyDouble;
+            decimal powderQtyDouble = 0.0M;
             try
             {
-                powderQtyDouble = Convert.ToDouble(powderQty.Text);
+                powderQtyDouble = Convert.ToDecimal(powderQty.Text);
+                _serialPort.WriteLine("TARA");
+                
+                new Thread(() => {
+                    Thread.Sleep(3000);
+                    while (Decimal.Compare(scaleValue, powderQtyDouble) < 0)
+                    {
+                        _serialPort.WriteLine("Add");
+                        Thread.Sleep(50);
+                    }
+                }).Start();
+
+                this.count++;
+                Stk.Content = count + " Stk";
             }
             catch {
                 MessageBox.Show("Ihre Eingabe ist keine Zahl", "Fehler im Zahlenformat");
+                return;
             }
-            this.count++;
-            Stk.Content = count + " Stk";
         }
 
         private void Reset_Button_Click(object sender, RoutedEventArgs e)
@@ -60,6 +96,11 @@ namespace AutoTricklerGui
                     return;
                 }
             }
+        }
+
+        private void taraBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _serialPort.WriteLine("TARA");
         }
     }
 }
